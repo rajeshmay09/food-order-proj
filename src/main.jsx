7eeb227042c46089c53d5f38ee0b2b757074e94a
rajeshmay09 <rajeshmayya09@gmail.com>
2024-07-26 13:11:26 +0530

    base

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "remixicon/fonts/remixicon.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
